ab9cb5edb25a69e4fad059e912e6a20f2b0e8c62
Mate Visky <matevisky@gmail.com>
2019-12-09 17:43:05 +1300

    update

--- 2019/computer.js
+++ 2019/computer.js
@@ -1,196 +1,206 @@
-const commands = {
-  1: c => executeCommand1(c),
-  2: c => executeCommand2(c),
-  3: c => executeCommand3(c),
-  4: c => executeCommand4(c),
-  5: c => executeCommand5(c),
-  6: c => executeCommand6(c),
-  7: c => executeCommand7(c),
-  8: c => executeCommand8(c),
-  99: c => c,
+const statuses = {
+  ERROR: -1,
+  NORMAL: 0,
+  INPUT: 3,
+  OUTPUT: 4,
+  END: 99,
 }
 
-const initComputer = (mainframe, params) => {
-  params = params || {};
-  const computer = {
-    id: params.id || 'default',
-    memory: [...(mainframe || [99])],
-    input: [],
-    output: [],
-    position: 0,
-    a: null,
-    b: null,
-    command: {},
-    debug: {
+const defaults = {
+  MAX_STEP: 999,
+  DEFAULT_COMPUTER_NAME: 'default',
+  // DEFAULT_MAINFRAME: [99],
+  DEFAULT_MAINFRAME: [98, 1, 98, 1, 98, 1, 99],
+}
+
+class Computer {
+  constructor(mainFrame = defaults.DEFAULT_MAINFRAME, params = {}) {
+    this.memory = [...mainFrame];
+    this.input = params.input || [];
+    this.output = [];
+    this.position = Number(params.position || 0) || 0;
+    this.a = null;
+    this.b = null;
+    this.command = {
+      opcode: null, ma: null, mb: null, mc: null};
+    this.status = statuses.NORMAL;
+    this.debug = {
+      id: params.id || defaults.DEFAULT_COMPUTER_NAME,
       currentStep: 0,
-      maxStep: params.maxStep || 9999
-    }
-  };
-
-  return computer;
-}
-
-const runComputer = (computer) => {
-  if (!computer.state) computer.state = "normal";
-
-  let i = 0;
-  do {
-    computer = runComputerStep(computer);
-    i++;
-  } while (computer.command.opcode != 99 && computer.command.opcode != 4 && i < 9999)
-  computer['i'] = i;
-
-  return computer
-}
-
-const runComputerStep = (computer) => {
-  if (computer.state != "input") {
-    computer = getNextCommand(computer)
-  }
-  computer = readParameters(computer)
-  if (computer.state == "input") return computer;
-  computer = executeCommand(computer)
-  return computer;
-}
-const executeCommand = computer => {
-  const fn = commands[computer.command.opcode];
-  if (fn) {
-    return fn(computer);
-  } else {
-    console.error(computer);
-    throw new Error("Unknown command!!!");
-  }
-}
-
-const executeCommand1 = computer => {
-  let mem = computer.memory;
-  let pos = computer.position;
-
-  mem[mem[pos + 3]] = computer.a + computer.b
-
-  computer.position += 4;
-  return computer;
-}
-
-const executeCommand2 = computer => {
-  let mem = computer.memory;
-  let pos = computer.position;
-
-  mem[mem[pos + 3]] = computer.a * computer.b
-
-  computer.position += 4;
-  return computer;
-}
-
-const executeCommand3 = computer => {
-  let mem = computer.memory;
-  let pos = computer.position;
-
-  mem[mem[pos + 1]] = computer.a;
-
-  computer.position += 2;
-  return computer;
-}
-
-const executeCommand4 = computer => {
-  computer.output.push(computer.a);
-
-  computer.position += 2;
-  return computer;
-}
-
-const executeCommand5 = computer => {
-  if (computer.a != 0) {
-    computer.position = computer.b
-  } else {
-    computer.position += 3;
+      maxStep: Number(params.maxStep || defaults.MAX_STEP) || 999,
+      message: "",
+    };
   }
 
-  return computer;
-}
+  * runComputer() {
+    do  {
+      this.debug.currentStep += 1;
+      if (this.isStackOverflow()) return this;
+      if (!this.getNextCommand()) return this;
 
-const executeCommand6 = computer => {
-  if (computer.a == 0) {
-    computer.position = computer.b
-  } else {
-    computer.position += 3;
-  }
-
-  return computer;
-}
-
-const executeCommand7 = computer => {
-  let mem = computer.memory;
-  let pos = computer.position;
-
-  mem[mem[pos + 3]] = computer.a < computer.b ? 1 : 0;
-
-  computer.position += 4;
-  return computer;
-}
-
-const executeCommand8 = computer => {
-  let mem = computer.memory;
-  let pos = computer.position;
-
-  mem[mem[pos + 3]] = computer.a == computer.b ? 1 : 0;
-
-  computer.position += 4;
-  return computer;
-}
-
-const readParameters = computer => {
-  let com = computer.command;
-  let mem = computer.memory;
-  let pos = computer.position;
-  let input = computer.input;
-
-  computer.a = null;
-  computer.b = null;
-
-  if ([3].includes(com.opcode)) {
-    if (input.length == 0) {
-      if (computer.state == "input") {
-        computer.state = "inputerror"
-        return computer;
+      const p = this.getParameters()
+      if (!p.next().value) {
+        yield this;
+        if (!p.next().value) return this;
       }
-      computer.state = "input";
-      return computer;
+
+      if (!this.executeCommand()) return this;
+    } while (this.command.opcode != 99)
+
+    return this;
+  }
+
+  pushInput(items) {
+    items = Array.isArray(items) ? items : [items];
+    this.input = [...this.input, ...items];
+  }
+
+  shiftOutput() {
+    return this.output.shift();
+  }
+
+  executeCommand() {
+    const commands = {
+      1: () => this.command1(),
+      2: () => this.command2(),
+      3: () => this.command3(),
+      4: () => this.command4(),
+      5: () => this.command5(),
+      6: () => this.command6(),
+      7: () => this.command7(),
+      8: () => this.command8(),
+      98: () => this.command98(),
+      99: () => this.command99(),
+      error: () =>this.commandError(),
     }
-    computer.a = input.shift()
-  } else if ([1, 2].includes(com.opcode)) {
-    computer.a = com.ma ? mem[pos + 1] : mem[mem[pos + 1]];
-    computer.b = com.mb ? mem[pos + 2] : mem[mem[pos + 2]];
-  } else {
-    computer.a = com.ma ? mem[pos + 1] : mem[mem[pos + 1]];
-    computer.b = com.mb ? mem[pos + 2] : mem[mem[pos + 2]];
+    let opcode = this.command.opcode;
+
+    if (!commands.hasOwnProperty(opcode)) opcode = 'error';
+    return commands[opcode]();
   }
-  computer.a = parseInt(computer.a)
-  computer.b = parseInt(computer.b)
 
-  return computer;
-}
-
-const peekNextCommand = computer => {
-  return getCommand(computer.memory[computer.position]);
-}
-
-const getNextCommand = computer => {
-  computer.command = getCommand(computer.memory[computer.position])
-  return computer
-}
-
-const getCommand = code => {
-  let codeStr = (code || "").toString();
-  while (codeStr.length < 5) {
-    codeStr = "0" + codeStr;
+  command1() {
+    this.memory[this.memory[this.position + 3]] = this.a + this.b
+    this.position += 4;
+    return true;
   }
-  let opcode = parseInt(codeStr.slice(-2));
-  let ma = parseInt(codeStr[2])
-  let mb = parseInt(codeStr[1])
-  let mc = parseInt(codeStr[0])
 
-  return { opcode, ma, mb, mc }
+  command2() {
+    this.memory[this.memory[this.position + 3]] = this.a * this.b
+    this.position += 4;
+    return true;
+  }
+
+  command3() {
+    this.memory[this.memory[this.position + 1]] = this.a;
+    this.position += 2;
+    return true;
+  }
+
+  command4() {
+    this.output.push(this.a);
+    this.position += 2;
+    return true;
+  }
+
+  command5() {
+    this.position = this.a == 0 ? this.position + 3 : this.b
+    return true;
+  }
+
+  command6() {
+    this.position = this.a != 0 ? this.position + 3 : this.b
+    return true;
+  }
+
+  command7() {
+    this.memory[this.memory[this.position + 3]] = this.a < this.b ? 1 : 0;
+    this.position += 4;
+    return true;
+  }
+
+  command8() {
+    this.memory[this.memory[this.position + 3]] = this.a == this.b ? 1 : 0;
+    this.position += 4;
+    return true;
+  }
+
+  command98() {
+    this.position += 2;
+    console.log('just a test');
+    return true;
+  }
+
+  command99() {
+    this.setStatus(statuses.END);
+    return true;
+  }
+
+  commandError() {
+    this.setStatus(statuses.ERROR, "Command not found error");
+    return false;
+  }
+
+  * getParameters() {
+    this.a = null;
+    this.b = null;
+
+    if (this.command.opcode == 3) {
+      if (this.input.length == 0) {
+        this.status = statuses.INPUT
+        yield false;
+        if (this.input.length == 0) {
+          this.setStatus(statuses.ERROR, "IO error input not recieved");
+          return false;
+        }
+
+      }
+      this.a = this.input.shift();
+      return true;
+    }
+    this.a = Number(this.command.ma ? this.memory[this.position + 1] : this.memory[this.memory[this.position + 1]]);
+    this.b = Number(this.command.mb ? this.memory[this.position + 2] : this.memory[this.memory[this.position + 2]]);
+
+    return true;
+  }
+
+  getNextCommand() {
+    if (this.position >= this.memory.length) {
+      this.setStatus(statuses.ERROR, `Pointer out of range`);
+      return false;
+    }
+    if (this.command.opcode == 99) return true;
+
+    let codeStr = String(this.memory[this.position] || "");
+    codeStr = "0".repeat(Math.max(0, 5 - codeStr.length)) + codeStr
+
+    let opcode = Number(codeStr.slice(-2));
+    let ma = Number(codeStr[2]);
+    let mb = Number(codeStr[1]);
+    let mc = Number(codeStr[0]);
+
+    this.command = { ...this.command, opcode, ma, mb, mc };
+    return true;
+  }
+
+  isStackOverflow() {
+    if (this.debug.currentStep > this.debug.maxStep) {
+      this.setStatus(statuses.ERROR, `Stack Overflow current step: ${this.debug.currentStep}`);
+      return true;
+    }
+    return false
+  }
+
+  setStatus(status, message = null) {
+    this.debug.message = message;
+    this.status = status;
+  }
 }
 
-module.exports = { initComputer };
+
+module.exports = {
+  Computer
+}
+
+
+
